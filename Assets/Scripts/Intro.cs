c87c051408962f3f9e667bf017d40c625a6e2cde
Katty Ptk <46168818+katty-ptk@users.noreply.github.com>
2022-08-31 09:48:00 +0300

    added writing animation and sound effect on intro text (#10)
    * added writing animation and sound effect on intro text
    
    * deleted typeWriterUI script

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -8,27 +8,54 @@ using DG.Tweening;
 public class Intro : MonoBehaviour
 {
 
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private List<Image> intro_images = new List<Image>();
-    [SerializeField] private TextMeshProUGUI story_text;
+    [SerializeField] private TextMeshProUGUI story_text, skip, click_to_learn;
 
     [SerializeField] private string[] story;
 
+    private Coroutine display_line_coroutine;
+
     private int image_index = -1;
     private string nextScene = "CharacterSelection";
+
+    private void Start(){
+        click_to_learn.DOFade(0.5f, 0.5f);
+        skip.DOFade(1f, 1f);
+    }
+
     public void SkipIntro()
     {
         SceneManager.LoadScene(nextScene);
     }
-    public void NextImage()
-    {
-        if (image_index == intro_images.Count - 1)
-        {
+
+    private IEnumerator DisplayLine( string line ) {
+        story_text.text = ""; // empty the text
+        audioSource.Play();
+
+        foreach ( char letter in line.ToCharArray() ) {
+            story_text.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void NextImage(){
+
+        if (image_index == intro_images.Count - 1){
             SkipIntro();
             return;
         }
 
+        if (click_to_learn.gameObject.active) click_to_learn.gameObject.SetActive(false);   // if player clicked once, the info text disappears
+
         image_index += 1;
         intro_images[image_index].DOFade(1f, 0.5f);
-        story_text.text = story[image_index];
+
+        // if line has not been completely shown then just skip it
+        if (display_line_coroutine != null){
+            StopCoroutine(display_line_coroutine);
+        }
+
+        display_line_coroutine = StartCoroutine(DisplayLine(story[image_index]));
     }
 }
