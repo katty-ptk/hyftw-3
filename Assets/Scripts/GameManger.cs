9476ad74f92f7a39ee891b5d86b83c57539298d1
Katty Lazar <katy.lazar05@gmail.com>
2022-09-03 14:34:06 +0300

    installed new terminal and testing it:)

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -13,5 +13,10 @@ public class GameManger : MonoBehaviour
         player.GetComponent<SpriteRenderer>().sprite = playersprite;
     }
 
-    
+    /*
+        pov: it's the 2000s and all the kids go crazy about the arcade games.
+        but in the best place with arcade game machines there is a machine that takes you to a new world
+        you only have to press a button
+        THE button…
+    */
 }
