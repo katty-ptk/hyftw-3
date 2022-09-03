c773cf7119b1a1baae1105842cbf3158f3c1418c
Katty Lazar <katy.lazar05@gmail.com>
2022-09-03 16:47:50 +0300

    fixed clicking on intro bug - added button element and deleted button component on canvas

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -12,11 +12,4 @@ public class GameManger : MonoBehaviour
         playersprite = selectedskin.GetComponent<SpriteRenderer>().sprite;
         player.GetComponent<SpriteRenderer>().sprite = playersprite;
     }
-
-    /*
-        pov: it's the 2000s and all the kids go crazy about the arcade games.
-        but in the best place with arcade game machines there is a machine that takes you to a new world
-        you only have to press a button
-        THE button…
-    */
 }
