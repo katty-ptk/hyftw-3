49ec3983ca05b5993fd9dad0490dc71e4b5a5680
unknown <olah.roland.denid@gmail.com>
2022-09-09 12:17:18 +0300

    am mai lucrat si eu pe aici

--- Assets/Scripts/Levels/Mario/MoveMario.cs
+++ Assets/Scripts/Levels/Mario/MoveMario.cs
@@ -9,6 +9,13 @@ private new Camera camera;
  private Rigidbody2D rb;
  private float InputAxis;
  public float speed = 8f;
+ public  bool Right = true;
+  public float maxJumpHeight = 5f;	
+  public float maxJumpTime = 1f;
+  public float JumpPower => (2f *maxJumpHeight) / (maxJumpTime /2f);
+  public float gravity => (-2f* maxJumpHeight) / Mathf.Pow((maxJumpTime /2f),2);
+  public bool grounded {get; private set;}
+  public bool jumping {get; private set;}
  private void Awake()
  {
     rb = GetComponent<Rigidbody2D>();
@@ -17,7 +24,32 @@ private new Camera camera;
 
  private void Update()
  {
-     HorizontalMove();
+    HorizontalMove();
+     Flip();
+    grounded = rb.Raycast(Vector2.down);
+    if(grounded)
+    {
+        GroundedMovement();
+    }
+    ApplyGravity();
+ }
+ private void ApplyGravity()
+ {
+    bool falling = velocity.y <0f || !Input.GetButtonDown("Jump");
+    float multiplier = (falling ? 2f : 1f);
+    velocity.y += gravity * Time.deltaTime;
+    velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+ }
+ private void GroundedMovement()
+ {
+    velocity.y = Mathf.Max(velocity.y, 0f);
+    jumping = velocity.y >0f;
+
+    if(Input.GetButtonDown("Jump"))
+    {
+        velocity.y = JumpPower;
+        jumping = true;
+    }
  }
  private void HorizontalMove()
  {
@@ -32,10 +64,20 @@ private  void FixedUpdate()
     
     Vector2 Stanga = camera.ScreenToWorldPoint(Vector2.zero);
     Vector2 Dreapta = camera.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
-    position.x = Mathf.Clamp(position.x,Stanga.x,Dreapta.x);
+    position.x = Mathf.Clamp(position.x,Stanga.x+0.5f,Dreapta.x-0.5f);
     rb.MovePosition(position);
 
 }
+private void Flip()
+    {
+        if ((Right && InputAxis< 0f) || (!Right && InputAxis> 0f))
+        {
+            Right = !Right;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
 
 
 }
