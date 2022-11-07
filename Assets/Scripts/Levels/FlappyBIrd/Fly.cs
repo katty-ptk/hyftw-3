de852e7c350e6f5cc65827970d74b36cfad217ee
unknown <olah.roland.denid@gmail.com>
2022-11-07 12:08:59 +0200

    cjrcb

--- Assets/Scripts/Levels/FlappyBIrd/Fly.cs
+++ Assets/Scripts/Levels/FlappyBIrd/Fly.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Fly : MonoBehaviour
 {
     public float velocity = 1;
+public AudioSource audioS;
+public AudioSource audioSGame;
+    public AudioClip hitsound;
+     public AudioClip flysound;
+     public AudioClip gamesound;
+
 //private bool dead = false;
 private Animator anim;
 public float  timer = 1f;
@@ -14,6 +20,10 @@ public float  timer = 1f;
      public FlyManager script;
     void Start()
     {
+        audioSGame.clip = gamesound;
+        audioSGame.Play();
+         audioSGame.loop = true;
+
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
        
@@ -24,7 +34,14 @@ public float  timer = 1f;
     void Update()
     {
         if(Input.GetButtonDown("Jump"))
-        rb.velocity = Vector2.up * velocity;
+        {
+        
+         rb.velocity = Vector2.up * velocity;
+         audioS.clip = flysound;
+        audioS.Play();
+
+        }
+       
     }
     void StopTime ()
     {
@@ -34,8 +51,10 @@ public float  timer = 1f;
 	{
 		if (collider.gameObject.tag == "Pipe")
 		{
-
+           audioS.clip = hitsound;
+        audioS.Play();
             script.GameOver();
+
 
         }
     }
